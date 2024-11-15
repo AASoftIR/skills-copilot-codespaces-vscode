a03ba6df1e3522a75ae1a94d219fc53af86de7f1
Alireza Mohebbi <aasofttests@gmail.com>
2024-11-15 08:29:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  return {
+    member: {
+      skills: {
+        skill: [
+          {
+            name: 'JavaScript',
+            level: 'advanced'
+          },
+          {
+            name: 'HTML',
+            level: 'advanced'
+          },
+          {
+            name: 'CSS',
+            level: 'advanced'
+          }
+        ]
+      }
+    }
+  }
+}
